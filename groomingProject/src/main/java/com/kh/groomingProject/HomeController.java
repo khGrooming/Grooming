19e3b3636aead3817bc52db28d5e0270352aa1d0
seung9430 <aiueos@naver.com>
2020-08-07 17:24:02 +0900

    commit

--- groomingProject/src/main/java/com/kh/groomingProject/HomeController.java
+++ groomingProject/src/main/java/com/kh/groomingProject/HomeController.java
@@ -1,3 +1,4 @@
+
 package com.kh.groomingProject;
 
 import java.text.DateFormat;
@@ -16,24 +17,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "home.do", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
+
 		model.addAttribute("serverTime", formattedDate );
-		
+
 		return "home";
 	}
 	
+	@RequestMapping("loginPage.do")
+	public String loginPage() {
+		
+		return "member/memberLoginRegistration";
+	}
+
 }
