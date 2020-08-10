d88b8e82d2c571976086e0f7c34f87b8ee0e2529
KoreanHoonie <gdoeb@naver.com>
2020-08-10 18:08:07 +0900

    승재형꺼 다시 머지 전 커밋

--- groomingProject/src/main/java/com/kh/groomingProject/member/model/vo/Member.java
+++ groomingProject/src/main/java/com/kh/groomingProject/member/model/vo/Member.java
@@ -9,6 +9,8 @@ public class Member implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = -7200797583466611023L;
+	
+	
 
 	private String memberNo;
 	private String memberEmail;
