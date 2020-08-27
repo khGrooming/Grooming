462b1afca368b1cc23bac0b4976ab01c0e413719
Helkanes <kangkwangsan@gmail.com>
2020-08-27 21:35:12 +0900

    ks_nb 승재 싱크함

--- groomingProject/src/main/java/com/kh/groomingProject/admin/model/service/AdminService.java
+++ groomingProject/src/main/java/com/kh/groomingProject/admin/model/service/AdminService.java
@@ -3,7 +3,9 @@ package com.kh.groomingProject.admin.model.service;
 import java.util.ArrayList;
 import java.util.Map;
 
+import com.kh.groomingProject.admin.model.vo.GroomingManageView;
 import com.kh.groomingProject.admin.model.vo.MemberManageView;
+import com.kh.groomingProject.admin.model.vo.MentoManageView;
 import com.kh.groomingProject.common.AdminPageInfo;
 import com.kh.groomingProject.studyCafe.model.vo.Point;
 
@@ -14,6 +16,21 @@ public interface AdminService {
 	ArrayList<MemberManageView> selectList(AdminPageInfo pi, Map info);
 
 	int pointCalculation(Point p);
+
+	int selectmentoCount(int i);
+
+	ArrayList<MentoManageView> selectmentoList(ArrayList<MemberManageView> mNo);
+
+	ArrayList<MentoManageView> selectSpareMentoList(ArrayList<MemberManageView> sNo);
+
+	ArrayList<MemberManageView> selectNo(AdminPageInfo pi, int i);
+
+	int selectGroomingCount(String category);
+
+	ArrayList<GroomingManageView> selectGroomingList(AdminPageInfo pi, String category);
+
+	
+
 
 	
 
