cf25c99842f5fbe17ef17775c6e1576bf64eff8b
Kwoneunjin <eunjin31101@gmail.com>
2020-09-07 16:13:04 +0900

    머지중

--- groomingProject/src/main/java/com/kh/groomingProject/community/model/service/CommunityService.java
+++ groomingProject/src/main/java/com/kh/groomingProject/community/model/service/CommunityService.java
@@ -2,13 +2,14 @@ package com.kh.groomingProject.community.model.service;
 
 import java.util.ArrayList;
 
+import com.kh.groomingProject.common.CommunityPageInfo;
 import com.kh.groomingProject.community.model.vo.Board;
 import com.kh.groomingProject.community.model.vo.Reply;
 import com.kh.groomingProject.member.model.vo.Member;
 
 public interface CommunityService {
 
-	ArrayList<Board> selectList(String bCategoryNo);
+	ArrayList<Board> selectList(String bCategoryNo, CommunityPageInfo pi);
 
 	Board selectOne(String boardNo);
 	
@@ -23,6 +24,20 @@ public interface CommunityService {
 	ArrayList<Reply> replySelectList(String boardNo);
 
 	int replyInsert(Reply reply, Member member);
+
+	ArrayList<Board> communityTopList();
+
+	ArrayList<Board> communitySearch(String communitySearch);
+
+	int getListCount(String bCategoryNo);
+
+	int replyUpdate(Reply reply, Member member);
+
+	int replyDelete(Reply reply, Member member);
+
+	
+
+	
 
 
 
